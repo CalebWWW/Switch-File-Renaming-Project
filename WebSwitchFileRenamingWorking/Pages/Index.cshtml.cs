145a62854a62ac069a678e38623e33c298627755
CalebWW <dutch910@icloud.com>
2023-09-16 23:38:49 -0500

    Deleted comments

--- WebSwitchFileRenamingWorking/Pages/Index.cshtml.cs
+++ WebSwitchFileRenamingWorking/Pages/Index.cshtml.cs
@@ -20,9 +20,9 @@ namespace WebSwitchFileRenamingWorking.Pages
         [BindProperty]
         public string LargeFileName { get; set; }
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8618
         public IndexModel(ILogger<IndexModel> logger)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning restore CS8618
         {
             _logger = logger;
             ResultStatus = "";
